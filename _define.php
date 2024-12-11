09ddc4bca50c7f8b567894ea9a2f5bdf6179daea
“philippe <philippe.mail.perso@gmail.com>
2024-12-11 11:50:21 +0100

    Update _define.php

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Admin More Dates',
     'Display entries creation or update dates',
     'Philippe aka amalgame and contributors',
-    '3.0.1',
+    '3.0.2',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
